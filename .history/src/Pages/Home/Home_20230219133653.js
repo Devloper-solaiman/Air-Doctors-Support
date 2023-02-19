9f3949daa317466dc0d680ef319f62e53cdf889f
Devloper-solaiman <devlopersolaiman@gmail.com>
2023-02-19 13:45:19 +0600

    MakeAppointment adn tastimonials

--- .history/src/Pages/Home/Home_20230219133653.js
+++ .history/src/Pages/Home/Home_20230219133653.js
@@ -4,7 +4,6 @@ import Banner from './Banner';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
-import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
@@ -13,7 +12,6 @@ const Home = () => {
             <Info></Info>
             <Services></Services>
             <MakeAppointment></MakeAppointment>
-            <Testimonials></Testimonials>
             <Footer></Footer>
         </section>
     );
---
+++ .history/src/Pages/Home/Testimonials_20230219134110.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import people1 from '../../assets/images/people1.png'
+import people2 from '../../assets/images/people2.png'
+import people3 from '../../assets/images/people3.png'
+import quote from '../../assets/icons/quote.svg'
+
+const Testimonials = () => {
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            review: '',
+            location: 'california',
+            img: people1
+        },
+        {
+            _id: 2,
+            name: 'Winson Herry',
+            review: '',
+            location: 'california',
+            img: people2
+        },
+        {
+            _id: 3,
+            name: 'Winson Herry',
+            review: '',
+            location: 'california',
+            img: people3
+        },
+    ];
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Testimonials;
